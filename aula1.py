2ca27dd68e731eac6999e02de64602323f674b05
Kalluan Cartoon <kalluanf@gmail.com>
2023-02-25 19:42:42 -0300

    commit inicial

--- aula1.py
+++ aula1.py
@@ -1,4 +1,11 @@
-print ('Hello, world')
+# (cerquilha) é usada para realizar comentario.
+print('123')
 
+"""
+DocString é usado para escrever as anotações porem não é um modo correto de deixar comentario
+"""
+'''
+Isso tambem é DocString.
+'''
 
 
