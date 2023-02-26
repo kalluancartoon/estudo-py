2ecab21ead1fb88000f64643ab02a7c98b87de33
Kalluan Cartoon <kalluanf@gmail.com>
2023-02-26 12:20:37 -0300

    explicações da aula 7

---
+++ aula7.py
@@ -0,0 +1,11 @@
+# Variáveis são usadas para salvar algo na memoria do computador.
+# PEP8: inicie variáveis com letras minusculas, pode usar numerosce underline -.
+#  O sinal de = é o operadorde atribuição. Ele é usado para atribuir um valor a um nome (variável).
+# Uso:nome_variavel = expressão
+
+nome = 'Kalluan Cartoon'
+idade = 28
+maior_de_idade = idade >= 18
+print('Nome:', nome )
+print('Idade:', idade)
+print('É maior?', maior_de_idade) 
