05ae006926b84be952d2a1c589d54915f6fac33f
Kalluan Cartoon <kalluanf@gmail.com>
2023-02-25 21:32:48 -0300

    explicações da aula 2 e 3

---
+++ aula2.py
@@ -0,0 +1,6 @@
+# Aprendendo a usar o comando print
+print(12, 34, sep='-') #sep='' é usado para acrescentar um separador. 
+print(21, 43, sep="-")
+
+
+
---
+++ aula3.py
@@ -0,0 +1,24 @@
+# Aula 3
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmico / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+print('123')
+
+# Aspas simples
+print('Kalluan Cartoon')
+print(1,'Kalluan "Cartoon"')
+
+
+# Aspas duplas
+print("Kalluan Cartoon")
+print(2,"Kalluan 'Cartoon'")
+
+
+# Escape
+print("Kalluan \"Cartoon\"")
+
+# r (é usado para expressões regulares)
+print( r"Kalluan \"Cartoon\"")
