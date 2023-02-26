d640733151390a90f42f1bb03556ce60a8ad2507
Kalluan Cartoon <kalluanf@gmail.com>
2023-02-26 08:37:51 -0300

    explicações da aula 5

---
+++ aula5.py
@@ -0,0 +1,12 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um prorama,
+# Só exixtem duasrespostas possiveis:
+# Sim (true) ou Não (False).
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que questiona se um valor é igusl ao outro. 
+
+print(10 == 10) #Sim => True (Verdadeiro)
+print(19 == 1) #Não => False (Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 30))
