2f053393a9a28680af1395995a48b6c98aa7217a
Kalluan Cartoon <kalluanf@gmail.com>
2024-01-12 20:42:06 -0300

    revisão 02

--- aula2.py
+++ aula2.py
@@ -1,4 +1,5 @@
 # Aprendendo a usar o comando print
+#Para duplicar a linha, clique nela e de ctrl+C, em seguida ctrl+V.
 print(12, 34, sep='-') #sep='' é usado para acrescentar um separador. 
 print(21, 43, sep="-")
 
