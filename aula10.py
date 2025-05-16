4b0db7c3a72f3fb3710e8652c00344d1fda9f085
Kalluan Cartoon <kalluanf@gmail.com>
2025-05-15 23:10:49 -0300

    aula 10

---
+++ aula10.py
@@ -0,0 +1,10 @@
+# Aula de concatenação
+# Concatenação é juntar, união dos valores.
+
+concatenacao = "A" + "B" + "C"
+print(concatenacao)
+
+a_dez_vezes = "A" * 10
+tres_vezes_kalluan = 3 * "Kalluan"
+print(a_dez_vezes)
+print(tres_vezes_kalluan)
